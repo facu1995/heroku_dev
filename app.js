fdfc807f5e1dc6ccb426124bd455b70158cd177a
Facundo <facuunion13@gmail.com>
2021-11-16 17:01:52 -0300

    utilizo form + uso de formato

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 /* import methodOverride from "method-override";
 import cors from "cors";
 import express from "express"; */
-const methodOverride =require("method-override");
+const methodOverride = require("method-override");
 const cors = require("cors");
-const express =require("express");
+const express = require("express");
 //
 const app = express();
 const log = console.log;
 
 let port = process.env.PORT || 3000;
 
-let users = ["bart", "lisa", "homero", "marge"]; 
+let users = ["bart", "lisa", "homero", "marge"];
 
 app.use(cors());
 app.use(methodOverride());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
-app.get("/users", (req, res)=>{
+app.get("/users", (req, res) => {
    res.send(users);
 });
 
-app.post("/user/create/:nombre", (req, res)=>{
-   users.push(req.params.nombre);
+app.post("/user/create/", (req, res) => {
+   users.push(req.body.nombre);
    res.send("usuario creado");
 });
 
-
- app.delete("/user/delete/:nombre", (req, res)=>{
-    users.filter(el =>el!=req.params.nombre)
-    res.send("usuario borrado");
- });
-app.put("/user/modify/:nombre/:nombre2",(req,res)=>{
-    users =users.map(el=>(req.params.nombre===el)? req.params.nombre2:el)
-    res.send("usuario ha sido modificado");
+app.delete("/user/delete/:nombre", (req, res) => {
+   users.filter(el => el != req.params.nombre)
+   res.send("usuario borrado");
+});
+app.put("/user/modify/:nombre/:nombre2", (req, res) => {
+   users = users.map(el => (req.params.nombre === el) ? req.params.nombre2 : el)
+   res.send("usuario ha sido modificado");
 })
 
-app.listen(port, ()=>{
-    log("start server");
+app.listen(port, () => {
+   log("start server");
 });
