43d6fa86a620e88bbb55c9b1a1fa79b9a90cc02b
Facundo <facuunion13@gmail.com>
2021-11-16 16:18:07 -0300

    Init

---
+++ app.js
@@ -0,0 +1,36 @@
+import methodOverride from "method-override";
+import cors from "cors";
+import express from "express";
+//
+const app = express();
+const log = console.log;
+
+let port = process.env.PORT || 3000;
+
+let users = ["bart", "lisa", "homero", "marge"]; 
+
+app.use(cors());
+app.use(methodOverride());
+
+app.get("/users", (req, res)=>{
+   res.send(users);
+});
+
+app.post("/user/create/:nombre", (req, res)=>{
+   users.push(req.params.nombre);
+   res.send("usuario creado");
+});
+
+
+ app.delete("/user/delete/:nombre", (req, res)=>{
+    users.filter(el =>el!=req.params.nombre)
+    res.send("usuario borrado");
+ });
+app.put("/user/modify/:nombre/:nombre2",(req,res)=>{
+    users =users.map(el=>(req.params.nombre===el)? req.params.nombre2:el)
+    res.send("usuario ha sido modificado");
+})
+
+app.listen(port, ()=>{
+    log("start server");
+});
