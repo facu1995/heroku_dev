f7a64035e0eaa9c79a9d8ddf18c61f807baf3aac
Facundo <facuunion13@gmail.com>
2021-11-19 17:24:33 -0300

    nuevo

--- app.js
+++ app.js
@@ -10,31 +10,76 @@ const log = console.log;
 
 let port = process.env.PORT || 3000;
 
-let users = ["bart", "lisa", "homero", "marge"];
-
 app.use(cors()); // permite conectar con servidores distintas
 app.use(methodOverride());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-   res.send(users);
-});
-
-app.post("/user/create/", (req, res) => {
-   users.push(req.body.nombre);
-   res.send("usuario creado");
-});
-
-app.delete("/user/delete/:nombre", (req, res) => {
-   users.filter(el => el != req.params.nombre)
-   res.send("usuario borrado");
-});
-app.put("/user/modify/:nombre/:nombre2", (req, res) => {
-   users = users.map(el => (req.params.nombre === el) ? req.params.nombre2 : el)
-   res.send("usuario ha sido modificado");
-})
 
 app.listen(port, () => {
    log("start server");
 });
+
+let users = [
+   { "email": "foo@foo.com", "name": "foo", "pass": "foo123" },
+   { "email": "bar@bar.com", "name": "bar", "pass": "bar123" },
+   { "email": "qux@qux.com", "name": "qux", "pass": "qux123" },
+];
+
+server.get("/users", (req, res) => {
+   res.send(users);
+});
+
+server.get("/reset", (req, res) => {
+   users = [];
+});
+
+server.get("/user/unmail/:email", (req, res) => {
+   res.send(users.filter(el => el.email == req.params.email));
+});
+
+server.get("/usersEmail/:email", (req, res) => {
+   let email = req.params.email;
+   let arrayEmail = email.split(",");
+   let resp = [];
+   arrayEmail.forEach((email) => {
+      users.forEach((user) => {
+         if (user.email == email) {
+            resp.push(user);
+         }
+      });
+   })
+   res.send(resp);
+});
+
+server.get("/user/name/", (req, res) => {
+   res.send(users.filter(el => (el.name === req.query.name)));
+});
+
+
+server.post("/user", (req, res) => {
+   users.push({ email: req.body.email, name: req.body.nombre, pass: req.body.pass });
+   res.send("usuario creado");
+});
+
+server.delete("/user/borrarUno/:email", (req, res) => {
+   users = users.filter(el => el.email != req.params.email);
+   res.send("usuario borrado");
+});
+
+server.delete("/user/borrarVarios/", (req, res) => {
+   let arrayEmail = req.query.email;
+   arrayEmail.forEach(email => {
+      users = users.filter((elemento) => elemento.email != email);
+   });
+   res.send("Se eliminaron todos los usuarios con ese mail");
+});
+
+server.put("/user/cambiar/", (req, res) => {
+   users = forEach((user, i) => {
+      if (user.email == req.body.email) {
+         users[i].email = req.body.nuevoEmail;
+      }
+   })
+   res.send("usuario ha sido modificado");
+})
